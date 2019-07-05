935952f8e1e13df7beddf1eb3f3fd54ca713cb98
ygl <lanyueos@qq.com>
2019-07-05 19:34:30 +0800

    添加类型

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,8 @@
 'use strict'
 import Router from 'koa-router'
+import { App } from './typings';
 
-export default function (app: { router: any; controller: any; }) {
+export default function (app: { router: Router; controller: App.Controller; }) {
   const { router, controller } = app
 
   router.get('/', controller.home.index)
