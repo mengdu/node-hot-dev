abf957b56f6a63aa0fc07815467d2f6c12c69186
ygl <lanyueos@qq.com>
2019-07-08 10:22:31 +0800

    添加出错处理

--- .dev/loader.js
+++ .dev/loader.js
@@ -2,20 +2,20 @@ const path = require('path')
 const Router = require('koa-router')
 const utils = require('./utils')
 
-function loadController (app, dir) {
+function loadController (app, dir, errorHandler) {
   const controllers = utils.loaderControllerFiles(path.resolve(dir), { extend: '.ts', isDeep: true })
   const dit = {}
 
   for (const key in controllers) {
     console.log('loader:', controllers[key], '-', key)
-    dit[key] = wrapClass(require(controllers[key]).default, app)
+    dit[key] = wrapClass(require(controllers[key]).default, app, errorHandler)
   }
 
   return dit
 }
 
 // copy from egg-core
-function wrapClass (Controller, app) {
+function wrapClass (Controller, app, errorHandler) {
 
   let proto = Controller.prototype
   const ret = {}
@@ -29,7 +29,7 @@ function wrapClass (Controller, app) {
       // skip getter, setter & non-function properties
       const d = Object.getOwnPropertyDescriptor(proto, key)
       if (utils.isFun(d.value) && !ret.hasOwnProperty(key)) {
-        ret[key] = methodToMiddleware(Controller, key, app)
+        ret[key] = methodToMiddleware(Controller, key, app, errorHandler)
       }
     }
     proto = Object.getPrototypeOf(proto)
@@ -37,23 +37,34 @@ function wrapClass (Controller, app) {
   return ret
 }
 
-function methodToMiddleware(Controller, key, app) {
+function methodToMiddleware(Controller, key, app, errorHandler) {
   return async function classControllerMiddleware(ctx, next) {
     const controller = new Controller({ app, ctx })
 
     try {
       await controller[key]()
     } catch (err) {
-      next(err)
+      if (utils.isFun(errorHandler)) errorHandler(err, ctx, next)
     }
   }
 }
 
+function errorHandler (err, ctx) {
+  ctx.status = 500
+  ctx.body = err.message
+  ctx.app.emit('error', err, ctx)
+}
+
 module.exports = function loader (app, options) {
-  options = { controllerDir: 'src/controller', routerFile: '../src/router', ...options }
+  options = { controllerDir: 'src/controller', routerFile: '../src/router', errorHandler: errorHandler, ...options }
   const router = new Router()
   const loadRouter = require(options.routerFile).default
 
-  loadRouter({ router: router, controller: loadController(app, options.controllerDir), application: app })
+  loadRouter({
+    router: router,
+    controller: loadController(app, options.controllerDir, options.errorHandler),
+    application: app
+  })
+
   app.use(router.routes()).use(router.allowedMethods())
 }
--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@
 import Koa from 'koa'
 import config from './config'
 import loader from '../.dev/loader'
+import logger from './middlewares/resquest-log'
 
 const app = new Koa()
 const port = config.port
 
+app.use(logger())
 app.use(async (ctx, next) => {
   ctx.config = config
 
@@ -14,6 +16,14 @@ app.use(async (ctx, next) => {
 
 // loader router
 loader(app)
+
+app.use(async function (ctx, next) {
+
+})
+
+app.on('error', (err, ctx) => {
+  console.error(err, ctx)
+})
 
 app.listen(port, function () {
   console.log('Listening on:', port)
